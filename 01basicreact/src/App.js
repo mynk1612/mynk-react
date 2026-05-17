fc5bb12d7237dca94c11fc58d72c4b17b9dce3ad
mynk1612 <mayankkandpal23@gmail.com>
2026-05-17 11:04:42 +0530

    react series

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>lens with mayank </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+ <h1> nature lens by mayank with vite | MK </h1>
+  )
+}
+
+export default App
