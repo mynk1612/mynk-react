54f728a4f076ce76af7dc35494ed2a58d8d36cc8
mynk1612 <mayankkandpal23@gmail.com>
2026-05-20 12:16:17 +0530

    own react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Lens from "./lensmynk"
 function App() {
+
+  const username = "nature lens by mayank "
   return (
     <>
- <h1> nature lens by mayank with vite | MK </h1> 
     <Lens />
+    <h1> nature lens by mayank with vite </h1> 
     <p> this is a nature lens by mayank with vite | MK </p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import {jsx as _jsx} from 'react/jsx-runtime'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App </h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//     },
+//     Children: 'Click me to visit Google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>
+    Visit Google 
+  </a>
+)
+const anotherUser = "lens with mynk"
+
+const reactElement = React.createElement(
+   'a',
+   {
+     href: 'https://google.com',
+     target: '_blank'
+   },
+   'Click me to visit Google',
+    anotherElement)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  /*
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ReactElement />
+  </StrictMode>,*/
+
+  // anotherElement,
+  // reactElement
+  // <App />
+  reactElement
 )
